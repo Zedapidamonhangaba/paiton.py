c58b06d9290c4490ce82347fa47b84c9d131981d
Dudu <129951560+Zedapidamonhangaba@users.noreply.github.com>
2024-05-06 08:32:46 -0300

    Create cadastro2.py

---
+++ cadastro2.py
@@ -0,0 +1,46 @@
+print("Faça o cadastro da primeira pessoa")
+cadastro1 = input('Digite seu nome: ')
+idade1 = int(input('Digite sua idade: ') )
+sexo1 = input('Digite seu sexo: ')
+
+
+print("Faça o cadastro da segunda pessoa")
+cadastro2 = input('Digite seu nome: ')
+idade2 = int(input('Digite sua idade: ') )
+sexo2 = input('Digite seu sexo: ')
+
+
+print("Faça o cadastro da terceira pessoa")
+cadastro3 = input('Digite seu nome: ')
+idade3 = int(input('Digite sua idade: ') )
+sexo3 = input('Digite seu sexo: ')
+
+
+print("Primeiro cadastro")
+print ("Nome:", cadastro1)
+print ("idade:", idade1)
+print ("sexo:", sexo1)
+if(idade1 >=18):
+    print("maior de idade")
+else:
+    print("menor de idade")
+
+
+print("Segundo cadastro")
+print ("Nome:", cadastro2)
+print ("idade:", idade2)
+print ("sexo:", sexo2)
+if(idade2 >=18):
+    print("maior de idade")
+else:
+    print("menor de idade")
+
+
+print("Terceiro cadastro")
+print ("Nome:", cadastro3)
+print ("idade:", idade3)
+print ("sexo:", sexo3)
+if(idade3 >=18):
+    print("maior de idade")
+else:
+    print("menor de idade")
