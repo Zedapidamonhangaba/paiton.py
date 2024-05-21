d347c031e7a68b9df1b80bf1cd60c9c5a73240f8
Dudu <129951560+Zedapidamonhangaba@users.noreply.github.com>
2024-05-21 10:54:57 -0300

    Create revisão.py

---
+++ revisão.py
@@ -0,0 +1,13 @@
+num1 = 5
+num2 = 5
+
+print("resultado:", num1 + num2)
+n3 = "8"
+n4 = "5"
+print ("resultado:", n3 + n4)
+print ("resultado:",int(n3)+int(n4))
+n5 = "8 "
+n6 = "5x" 
+print("resultado:", n5 + n6)
+n8 = input("Digite um valor")
+print ("Resultado:", n8+"5")
