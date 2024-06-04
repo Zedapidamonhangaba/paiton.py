2051bdd16d44f2575b6d17af1d97f7168e7511c9
Dudu <129951560+Zedapidamonhangaba@users.noreply.github.com>
2024-06-04 10:57:38 -0300

    Update Jogodaforca.py

--- Jogodaforca.py
+++ Jogodaforca.py
@@ -1,33 +1,35 @@
 def Forca(tentativa):
-    f1 = "     +--------+ "
-    f2 = "     |          "
-    f3 = "     |          "
-    f4 = "     |          " 
-    f5 = "     |          "
-    f6 = "     |          "
-    f7 = "_____|____      "
+    f1 = "  +------------+ "
+    f2 = "  |              "
+    f3 = "  |              "
+    f4 = "  |              "
+    f5 = "  |              "
+    f6 = "  |              "
+    f7 = "__|______________"
 
     if tentativa >= 1:
-        f2 = " |        | "
+        f2= "  |            | "
+
     if tentativa >= 2:
-        f3 = " |        O "
-    if tentativa >= 3:
-        f4 = " |        | "
-    if tentativa >= 4:
-        f4 = " |       /| "
-    if tentativa >= 5:
-        f4 = " |       /|\ "
-    if tentativa >= 6:
-        f5 = " |        | "
+        f3 = "  |            O " 
+
+    if tentativa >= 3:  
+        f4 = "  |            | "
+
+    if tentativa >= 4: 
+        f4 = "  |           /| "
+
+    if tentativa >= 5: 
+        f4 = "  |           /|\ "
+
+    if tentativa >= 6: 
+        f5 = "  |            |  "
+
     if tentativa >= 7:
-        f6 = " |       / "
+        f6 = "  |           /  "
+
     if tentativa >= 8:
-        f6 = " |       / \ "
-    
-    
-    
-
-
+        f6 = "  |           / \ "  
 
     print(f1)
     print(f2)
@@ -36,27 +38,36 @@ def Forca(tentativa):
     print(f5)
     print(f6)
     print(f7)
-    
-Forca(0)
 
-def Continue():
-  while True:
-        print("" * 20)
-        novamete = input("Quer jogar de novo S/N: ").upper()
-        if novamete == "S":
+def Sorteiapalavra():
+    lista =["Osvaldo", "Transformes", "Vidro", "Chantagem", "Zumbi", "Coroa", "Camaro", "Mustang", "Estelionato", "Athletico"]
+    return random.choice(lista)
+
+
+
+def Continua():
+    while True:
+        print("-" * 20)
+        novamente = input("Quer Jogar de S/N:").upper()
+        if novamente == "S":
             Acabou = True
             break
-        elif novamete == "N":
+        elif novamente == "N":
             Acabou = False
             break
         else:
-            print("Digite S ou N")
-  return Acabou 
-
+            print("Digite S ou N: ")
+    return Acabou
 Jogar = True
 x=0
-while Jogar :
-    print(x)
-    Forca(x)
-    Jogar = Continue()
-    x = x + 1 
+
+#while Jogar:
+    #print(x)
+    #Forca(x)
+    #Jogar = Continua()
+    #x = x + 1
+
+Forca(0)
+
+import random
+print(Sorteiapalavra())
