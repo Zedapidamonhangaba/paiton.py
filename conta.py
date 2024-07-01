c6e0f4723ae9718e65ebf1a7c0ff63219c8e3042
Dudu <129951560+Zedapidamonhangaba@users.noreply.github.com>
2024-07-01 08:54:19 -0300

    Create conta.py

---
+++ conta.py
@@ -0,0 +1,16 @@
+def cria_conta(numero,titular,saldo,limite):
+    conta={"numero":numero,"titular":titular,"saldo":saldo,"limite":limite
+}
+    return conta
+def saldo(conta):
+        print("contanumero:{}".format(conta["numero"]))
+        print("titular:"+ conta["titular"])
+        print("saldo:{}".format(conta["saldo"]))
+
+def deposito(conta,valor):
+    conta+=valor
+
+conta00 = cria_conta(1234,"Eduardo", 50.0, 100.0)
+conta01 = cria_conta(4321,"Rodolfo", 50.0, 100.0)
+saldo(conta00)
+saldo(conta01)
