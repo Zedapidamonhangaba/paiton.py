bd4e5e980639f545d54b44367c1395d87eb65816
Dudu <129951560+Zedapidamonhangaba@users.noreply.github.com>
2024-07-02 10:35:50 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -14,3 +14,15 @@ conta00 = cria_conta(1234,"Eduardo", 50.0, 100.0)
 conta01 = cria_conta(4321,"Rodolfo", 50.0, 100.0)
 saldo(conta00)
 saldo(conta01)
+
+def extrato(conta):
+    print("conta{}saldo{}".format(conta["numero"],conta["saldo"]))
+def deposito(conta,valor):
+    conta["saldo"]+=valor
+def sacar(conta,valor):
+    conta["saldo"]-=valor
+    deposito(conta)
+deposito(conta00, 100.0)
+extrato(conta00)
+deposito(conta01, 100.0)
+extrato(conta01)
